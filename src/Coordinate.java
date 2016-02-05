ce58c6023d4ce18993526bda8723ac3ca06a863b
verlaqd <verlaqd@rose-hulman.edu>
2016-02-05 13:45:39 -0500

    Created Coordinate class framework. Not sure what the UML diagram means for the time() method

---
+++ src/Coordinate.java
@@ -0,0 +1,37 @@
+
+public class Coordinate {
+
+	protected double x;
+	protected double y;
+	
+	/**
+	 * Create a new Coordinate at (0,0) if no parameters are supplied
+	 */
+	public Coordinate(){
+		this(0,0);
+	}
+	
+	/**
+	 * Create a new Coordinate at (x,y)
+	 * @param x
+	 * @param y
+	 */
+	public Coordinate(double x, double y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	/**
+	 * Return the straight-line distance to another coordinate
+	 *
+	 * @param other
+	 * @return
+	 */
+	public double distance(Coordinate other){
+		double xdist = Math.abs(this.x-other.x);
+		double ydist = Math.abs(this.y-other.y);
+		return Math.hypot(xdist, ydist);
+//		return Math.sqrt(xdist*xdist+ydist*ydist);
+	}
+	
+}
