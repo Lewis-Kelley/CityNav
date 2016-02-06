5ea0c0743c66cf5779ea66f28b95cb5a6652c9c5
verlaqd <verlaqd@rose-hulman.edu>
2016-02-06 14:42:14 -0500

    Added demo code to test generateChildren() and Path() functionality. Some starter lines for creating panels, etc. for GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,43 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		//TODO Write everything
-		//test
+		JFrame CityNavFrame = new JFrame();
+		
+		JPanel CityNavPanel = new JPanel();
+		CityNavFrame.add(CityNavPanel);
+		
+		JPanel ControlsPanel = new JPanel();
+		CityNavPanel.add(ControlsPanel);
+		
+		JPanel PathfinderPanel = new JPanel();
+		ControlsPanel.add(PathfinderPanel);
+		
+		
+		
+		
+		CityNavFrame.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void demo(){
+		Node one = new Node("Node1", new Coordinate(0, 0), 50);
+		Node two = new Node("Node2", new Coordinate(0, 50), 75);
+		Node three = new Node("Node3", new Coordinate(50, 50), 95);
+		
+		Path pathOneTwo = new Path(one,two,60,1);
+		Path pathOneThree = new Path(one,three,80,1.25);
+		System.out.println(one.generateChildren(false, 0));
 	}
 
 }
