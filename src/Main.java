f5682910579954028a7d179f73f98bd70cbf91a1
kelleyld <kelleyld@rose-hulman.edu>
2016-02-11 10:37:00 -0500

    Added a little more to the GUI, including the Component.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
 public class Main {
 
@@ -15,78 +16,94 @@ public class Main {
 		JPanel CityNavPanel = new JPanel(new BorderLayout());
 		CityNavFrame.add(CityNavPanel);
 		{
+			GraphAndOutputComponent gr = new GraphAndOutputComponent();
+			CityNavPanel.add(gr, BorderLayout.EAST);
+			
 			JPanel ControlsPanel = new JPanel(new BorderLayout());
 			CityNavPanel.add(ControlsPanel, BorderLayout.WEST);
 			{
-				JButton InterestButton = new JButton("View Cities By Interest");
-				ControlsPanel.add(InterestButton, BorderLayout.NORTH);
-				
-				JPanel PathfinderPanel = new JPanel(new BorderLayout());
-				ControlsPanel.add(PathfinderPanel, BorderLayout.CENTER);
+				JPanel ControlsNorthPanel = new JPanel(new BorderLayout());
+				ControlsPanel.add(ControlsNorthPanel, BorderLayout.NORTH);
 				{
-					JPanel PathfinderLabelsPanel = new JPanel(new BorderLayout());
-					PathfinderPanel.add(PathfinderLabelsPanel, BorderLayout.NORTH);
+					JButton InterestButton = new JButton("View Cities By Interest");
+					ControlsNorthPanel.add(InterestButton, BorderLayout.NORTH);
+
+					JPanel PathfinderPanel = new JPanel(new BorderLayout());
+					ControlsNorthPanel.add(PathfinderPanel, BorderLayout.SOUTH);
 					{
-						JLabel PathfinderStartLabel = new JLabel("Start");
-						PathfinderLabelsPanel.add(PathfinderStartLabel, BorderLayout.WEST);
-						
-						JLabel PathfinderEndLabel = new JLabel("End");
-						PathfinderLabelsPanel.add(PathfinderEndLabel, BorderLayout.EAST);
+						JPanel PathfinderLabelsPanel = new JPanel(new BorderLayout());
+						PathfinderPanel.add(PathfinderLabelsPanel, BorderLayout.NORTH);
+						{
+							JLabel PathfinderStartLabel = new JLabel("Start");
+							PathfinderLabelsPanel.add(PathfinderStartLabel, BorderLayout.WEST);
+
+							JLabel PathfinderEndLabel = new JLabel("End");
+							PathfinderLabelsPanel.add(PathfinderEndLabel, BorderLayout.EAST);
+						}
+
+						JPanel PathfinderInputPanel = new JPanel(new BorderLayout());
+						PathfinderPanel.add(PathfinderInputPanel, BorderLayout.CENTER);
+						{
+							JTextField PathfinderStart = new JTextField(10);
+							PathfinderInputPanel.add(PathfinderStart, BorderLayout.WEST);
+
+							JTextField PathfinderEnd = new JTextField(10);
+							PathfinderInputPanel.add(PathfinderEnd, BorderLayout.EAST);
+						}
+
+						JPanel PathfinderButtonsPanel = new JPanel(new BorderLayout());
+						PathfinderPanel.add(PathfinderButtonsPanel, BorderLayout.SOUTH);
+						{
+							JButton PathfinderTimeButton = new JButton("   Time   ");
+							PathfinderButtonsPanel.add(PathfinderTimeButton, BorderLayout.EAST);
+
+							JButton PathfinderDistanceButton = new JButton("Distance");
+							PathfinderButtonsPanel.add(PathfinderDistanceButton, BorderLayout.WEST);
+						}
 					}
-					
-					JPanel PathfinderInputPanel = new JPanel(new BorderLayout());
-					PathfinderPanel.add(PathfinderInputPanel, BorderLayout.CENTER);
-					{
-						JTextField PathfinderStart = new JTextField(10);
-						PathfinderInputPanel.add(PathfinderStart, BorderLayout.WEST);
-						
-						JTextField PathfinderEnd = new JTextField(10);
-						PathfinderInputPanel.add(PathfinderEnd, BorderLayout.EAST);
-					}
-					
-					JPanel PathfinderButtonsPanel = new JPanel(new BorderLayout());
-					PathfinderPanel.add(PathfinderButtonsPanel, BorderLayout.SOUTH);
-					{
-						JButton PathfinderTimeButton = new JButton("   Time   ");
-						PathfinderButtonsPanel.add(PathfinderTimeButton, BorderLayout.EAST);
-						
-						JButton PathfinderDistanceButton = new JButton("Distance");
-						PathfinderButtonsPanel.add(PathfinderDistanceButton, BorderLayout.WEST);
-					}
+
 				}
-				
-				JPanel PlannerPanel = new JPanel(new BorderLayout());
-				ControlsPanel.add(PlannerPanel, BorderLayout.SOUTH);
+
+				JPanel ControlsSouthPanel = new JPanel(new BorderLayout());
+				ControlsPanel.add(ControlsSouthPanel, BorderLayout.SOUTH);
 				{
-					JPanel PlannerLabelsPanel = new JPanel(new BorderLayout());
-					PlannerPanel.add(PlannerLabelsPanel, BorderLayout.NORTH);
+					JPanel PlannerPanel = new JPanel(new BorderLayout());
+					ControlsSouthPanel.add(PlannerPanel, BorderLayout.NORTH);
 					{
-						JLabel PlannerStartLabel = new JLabel("Start");
-						PlannerLabelsPanel.add(PlannerStartLabel, BorderLayout.WEST);
-						
-						JLabel PlannerEndLabel = new JLabel("Amount");
-						PlannerLabelsPanel.add(PlannerEndLabel, BorderLayout.EAST);
+						JPanel PlannerLabelsPanel = new JPanel(new BorderLayout());
+						PlannerPanel.add(PlannerLabelsPanel, BorderLayout.NORTH);
+						{
+							JLabel PlannerStartLabel = new JLabel("Start");
+							PlannerLabelsPanel.add(PlannerStartLabel, BorderLayout.WEST);
+
+							JLabel PlannerEndLabel = new JLabel("Amount");
+							PlannerLabelsPanel.add(PlannerEndLabel, BorderLayout.EAST);
+						}
+
+						JPanel PlannerInputPanel = new JPanel(new BorderLayout());
+						PlannerPanel.add(PlannerInputPanel, BorderLayout.CENTER);
+						{
+							JTextField PlannerStart = new JTextField(10);
+							PlannerInputPanel.add(PlannerStart, BorderLayout.WEST);
+
+							JTextField PlannerEnd = new JTextField(10);
+							PlannerInputPanel.add(PlannerEnd, BorderLayout.EAST);
+						}
+
+						JPanel PlannerButtonsPanel = new JPanel(new BorderLayout());
+						PlannerPanel.add(PlannerButtonsPanel, BorderLayout.SOUTH);
+						{
+							JButton PlannerTimeButton = new JButton("   Time   ");
+							PlannerButtonsPanel.add(PlannerTimeButton, BorderLayout.EAST);
+
+							JButton PlannerDistanceButton = new JButton("Distance");
+							PlannerButtonsPanel.add(PlannerDistanceButton, BorderLayout.WEST);
+						}
 					}
-					
-					JPanel PlannerInputPanel = new JPanel(new BorderLayout());
-					PlannerPanel.add(PlannerInputPanel, BorderLayout.CENTER);
-					{
-						JTextField PlannerStart = new JTextField(10);
-						PlannerInputPanel.add(PlannerStart, BorderLayout.WEST);
-						
-						JTextField PlannerEnd = new JTextField(10);
-						PlannerInputPanel.add(PlannerEnd, BorderLayout.EAST);
-					}
-					
-					JPanel PlannerButtonsPanel = new JPanel(new BorderLayout());
-					PlannerPanel.add(PlannerButtonsPanel, BorderLayout.SOUTH);
-					{
-						JButton PlannerTimeButton = new JButton("   Time   ");
-						PlannerButtonsPanel.add(PlannerTimeButton, BorderLayout.EAST);
-						
-						JButton PlannerDistanceButton = new JButton("Distance");
-						PlannerButtonsPanel.add(PlannerDistanceButton, BorderLayout.WEST);
-					}
+
+					JTextPane OutputText = new JTextPane();
+					OutputText.setText("This is where output will go.\n\n\n\n\n\n\n");
+					ControlsSouthPanel.add(OutputText, BorderLayout.SOUTH);
 				}
 			}
 		}
